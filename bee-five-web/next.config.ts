5f2d16a18b024b40b389f5ab9a650b57f577d533
https://github.com/TheCyberAyo <ndamaseayongezwa@gmail.com>
2026-04-23 16:54:25 +0200

    chore: update version to 1.0.1+18 and add flutter_icons configuration

--- bee-five-web/next.config.ts
+++ bee-five-web/next.config.ts
@@ -1,7 +1,12 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/app-ads.txt",
+        destination: "/public/app-ads.txt",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
